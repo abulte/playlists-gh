e544396425e91a7497f0fabae8da43ed12eeb13a
Alexandre Bulté <alexandre@bulte.net>
2020-06-23 09:01:47 +0200

    Initial commit

---
+++ scripts/to_api.py
@@ -0,0 +1,44 @@
+"""
+Parse playlists/*.toml and outputs "API" files to `build/`
+
+Usage:
+$ python scripts/to_api.py
+"""
+import json
+import os
+import toml
+import requests
+from pathlib import Path
+
+# dataset attributes to expose on our "API"
+DATASET_ATTRS = [
+    "id",
+    "title",
+    "description",
+    "owner",
+    "organization"
+]
+OUT_DIR = os.getenv('API_OUT_DIR', 'build/')
+
+path = Path("playlists/")
+
+for playlist in path.glob("*.toml"):
+    data = toml.load(playlist)
+    datasets = []
+    for did in data.get("datasets", []):
+        r = requests.get(f"https://www.data.gouv.fr/api/1/datasets/{did}")
+        if r.ok:
+            d_data = r.json()
+            dataset = {k: d_data.get(k) for k in DATASET_ATTRS}
+            datasets.append(dataset)
+        else:
+            print(f"Wrong response ({r.status_code}) for '{did}'")
+    playlist_api = {
+        "slug": playlist.stem,
+        "title": data.get("title"),
+        "datasets": datasets
+    }
+    Path(OUT_DIR).mkdir(exist_ok=True, parents=True)
+    out_path = Path(OUT_DIR) / f'{playlist.stem}.json'
+    with out_path.open('w') as out_file:
+        out_file.write(json.dumps(playlist_api))
